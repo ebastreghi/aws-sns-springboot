eb60d0b5de294eb94ea8aedf0edb08ccee2df81d
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2021-03-11 16:44:22 -0300

    Initial project
    Initial project

---
+++ src/main/java/com/awssnsspringboot/AwsSnsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.awssnsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSnsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSnsSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awssnsspringboot/AwsSnsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awssnsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSnsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
